1c805378615aeaed0e59fbc6cca5b25ee7d27f19
MaxChipov <lbaralgeen@gmail.com>
2018-12-13 12:01:16 -0500

    Update UNIT_TESTS\replacedb.py
    Make code simply, no need to use MS SQL to generate new name.

--- UNIT_TESTS\replacedb.py
+++ UNIT_TESTS\replacedb.py
@@ -1,7 +1,7 @@
 import os
 import io
 import sys
-import pymssql
+import uuid
 import codecs
 import configparser
 
@@ -11,19 +11,8 @@ settings.read('config.ini')
 infilename=settings.get('app','script')
 outfilename='scriptdb.sql'
 originaldb=settings.get('app','origindb')
-newdbdb=originaldb
-
-# -------------------   get the name of last created database ----------------------
-conn = pymssql.connect(server=settings.get('app','server'), database=r'master')
-cursor = conn.cursor()
-cursor.execute("SELECT LEFT(replace(CAST(newid() AS sysname), '-', ''), 8) AS name")
-row = cursor.fetchone()
-while row:
-    newdbdb = row[0]
-    row = cursor.fetchone()
-    break
-conn.close()
-
+newdbdb=str(uuid.uuid4())
+newdbdb=newdbdb[:8]
 print newdbdb
 
 # -------------------   replace template database name with last created ----------------------
